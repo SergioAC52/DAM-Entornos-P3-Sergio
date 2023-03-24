e48da3ee3bd51863426fcfa9ba066e007b780c1a
TilinExe816 <127988580+TilinExe816@users.noreply.github.com>
2023-03-24 18:30:06 +0100

    Update StringUtils.java

--- src/main/java/ieseuropa/StringUtils.java
+++ src/main/java/ieseuropa/StringUtils.java
@@ -2,7 +2,7 @@ package main.java.ieseuropa;
 
 public class StringUtils {
 
-	private static String repetirVeces(String string, int veces) {
+private static String repetirVeces(String string, int veces) {
 
 		String texto = "";
 
@@ -17,4 +17,43 @@ public class StringUtils {
 	}
 
 
-}
+private static String repetirTodo(String palabra) {
+
+		String resultado = "";
+
+		for (int i = 0; i < palabra.length(); i++) {
+
+			for (int j = 0; j < 2; j++) {
+
+				resultado = resultado + palabra.charAt(i);
+
+			}
+
+		}
+
+		return resultado;
+	}
+
+	private static int ocurrencias(String palabra, char letra) {
+
+		int contador = 0;
+
+		for (int i = 0; i < palabra.length(); i++) {
+
+			if (palabra.charAt(i) == letra) {
+
+				contador++;
+
+			}
+
+		}
+
+		return contador;
+	}
+	public static void main(String[] args) {
+
+		System.out.println(repetirTodo("hola"));
+		System.out.print(ocurrencias("hola",'a'));
+		System.out.print(repetirVeces("hola",5));
+	}
+	}
