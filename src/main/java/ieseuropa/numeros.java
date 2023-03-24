8c7567e6f916509338a144ca49668aa46452b332
TilinExe816 <nuevaunion22@gmail.com>
2023-03-24 20:06:50 +0100

    Suerte codigo

--- src/main/java/ieseuropa/numeros.java
+++ src/main/java/ieseuropa/numeros.java
@@ -1,36 +1,41 @@
 
 package main.java.ieseuropa;
 
+import java.util.ArrayList;
+
 public class numeros {
 
-	private static String Pell() {
+	public static boolean suerte(int numero) {
 
-		String resultado = "";
-
-		int numeroPrec = 2;
-
-		int numeroAnte = 1;
-
-		for (int i = 1; i < 16; i++) {
-
-			int aux = numeroAnte;
-
-			numeroAnte = numeroPrec;
-
-			numeroPrec = aux + (2 * (numeroPrec));
-
-			resultado = resultado + " " + numeroPrec;
-
-			
+		ArrayList<Integer> numeros = new ArrayList<>();
+		for (int i = 1; i <= numero; i++) {
+			numeros.add(i);
 		}
-		return resultado;
+
+		int contador = 1;
+		while (contador < numeros.size()) {
+			for (int j = contador; j < numeros.size(); j += contador) {
+				numeros.remove(j);
+
+			}
+			contador++;
+		}
+
+		if (numeros.contains(numero)) {
+
+			return true;
+
+		}
+
+		return false;
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Pell());
+
+		System.out.print(suerte(25));
 
 	}
 
 }
-
