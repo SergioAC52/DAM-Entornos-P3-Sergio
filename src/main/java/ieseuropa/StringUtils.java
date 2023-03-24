4f942ff2b46d66a872c0a76c4c956822d55f91e8
TilinExe816 <nuevaunion22@gmail.com>
2023-03-24 16:25:52 +0100

    Identado

--- src/main/java/ieseuropa/StringUtils.java
+++ src/main/java/ieseuropa/StringUtils.java
@@ -1,33 +1,28 @@
 package main.java.ieseuropa;
 
 public class StringUtils {
-	
+
 	private static String repetirTodo(String palabra) {
-		
+
 		String resultado = "";
-		
+
 		for (int i = 0; i < palabra.length(); i++) {
 
-			for(int j=0; j<2 ; j++) {
-				 
-				
+			for (int j = 0; j < 2; j++) {
+
 				resultado = resultado + palabra.charAt(i);
-				
-			}
-			
 
 			}
 
-		
+		}
 
 		return resultado;
 	}
 
-
 	public static void main(String[] args) {
-	
-	System.out.print(repetirTodo("hola"));	
-		
+
+		System.out.print(repetirTodo("hola"));
+
 	}
 
 }
