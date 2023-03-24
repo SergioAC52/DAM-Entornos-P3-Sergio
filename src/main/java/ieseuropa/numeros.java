0a23369a550081e30fbf8f9f63c8c8f051d7c785
TilinExe816 <nuevaunion22@gmail.com>
2023-03-24 20:35:05 +0100

    metodo para maximos

--- src/main/java/ieseuropa/numeros.java
+++ src/main/java/ieseuropa/numeros.java
@@ -4,62 +4,35 @@ package main.java.ieseuropa;
 import java.util.ArrayList;
 
 public class numeros {
-
-	private static String Pell() {
-
-		String resultado = "";
-
-		int numeroPrec = 2;
-
-		int numeroAnte = 1;
-
-		for (int i = 1; i < 16; i++) {
-
-			int aux = numeroAnte;
-
-			numeroAnte = numeroPrec;
-
-			numeroPrec = aux + (2 * (numeroPrec));
-
-			resultado = resultado + " " + numeroPrec;
-
-
-		}
-		return resultado;
-	}
-
-
-	public static boolean suerte(int numero) {
-
-		ArrayList<Integer> numeros = new ArrayList<>();
-		for (int i = 1; i <= numero; i++) {
-			numeros.add(i);
-		}
-
-		int contador = 1;
-		while (contador < numeros.size()) {
-			for (int j = contador; j < numeros.size(); j += contador) {
-				numeros.remove(j);
-
+	
+	private static int maximo(int[]numeros ) {
+		
+		int max=0;
+		
+		for(int i=0; i<numeros.length; i++) {
+			
+			if(numeros[i] > max) {
+				
+				max = numeros[i];
+				
 			}
-			contador++;
+			
+			
 		}
-
-		if (numeros.contains(numero)) {
-
-			return true;
-
-		}
-
-		return false;
-
+		
+		
+		return max;
+		
+		
 	}
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print(suerte(25));
-		System.out.print(Pell());
+		int[]nums = {2,6,1,6,10};
+		
+		System.out.print(maximo(nums));
 	}
 
 }
