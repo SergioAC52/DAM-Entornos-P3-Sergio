5878c1c8cf61ef71b549be6e8631b63db2792e22
TilinExe816 <127988580+TilinExe816@users.noreply.github.com>
2023-03-24 20:27:18 +0100

    Update StringUtils.java

--- src/main/java/ieseuropa/StringUtils.java
+++ src/main/java/ieseuropa/StringUtils.java
@@ -3,6 +3,56 @@ package main.java.ieseuropa;
 
 public class StringUtils {
 	
+	private static String repetirVeces(String string, int veces) {
+
+		String texto = "";
+
+		for (int i = 0; i < veces; i++) {
+
+			texto = texto + " " + string;
+
+		}
+		
+		return texto;
+	}
+
+
+
+	private static String repetirTodo(String palabra) {
+
+		String resultado = "";
+
+		for (int i = 0; i < palabra.length(); i++) {
+
+			for (int j = 0; j < 2; j++) {
+
+				resultado = resultado + palabra.charAt(i);
+
+			}
+
+
+		}
+
+		return resultado;
+	}
+
+	private static int ocurrencias(String palabra, char letra) {
+
+		int contador = 0;
+
+		for (int i = 0; i < palabra.length(); i++) {
+
+			if (palabra.charAt(i) == letra) {
+
+				contador++;
+
+			}
+
+		}
+
+		return contador;
+	}
+	
 	private static String mayus(String palabra) {
 
 		return palabra.toUpperCase();
@@ -14,12 +64,19 @@ public class StringUtils {
 		return palabra.toLowerCase();
 
 	}
-
-
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println(repetirTodo("hola"));
+		System.out.print(ocurrencias("hola",'a'));
+		System.out.print(repetirVeces("hola",5));
 		System.out.println(mayus("hola"));
 		System.out.println(minus("HOLA"));
 	}
+	
+
+
+	
 }
 
