ae8f408997957ff8d437ff0501f0d0d304d7ab94
TilinExe816 <nuevaunion22@gmail.com>
2023-03-24 20:22:31 +0100

    mayus-Minus codigo

--- src/main/java/ieseuropa/StringUtils.java
+++ src/main/java/ieseuropa/StringUtils.java
@@ -1,62 +1,25 @@
-@@ -1,59 +1,25 @@
+
 package main.java.ieseuropa;
 
 public class StringUtils {
 	
+	private static String mayus(String palabra) {
 
-private static String repetirVeces(String string, int veces) {
-
-		String texto = "";
-
-		for (int i = 0; i < veces; i++) {
-
-			texto = texto + " " + string;
-
-		}
-
+		return palabra.toUpperCase();
 
 	}
 
-	
+	private static String minus(String palabra) {
 
-private static String repetirTodo(String palabra) {
+		return palabra.toLowerCase();
 
-		String resultado = "";
-
-		for (int i = 0; i < palabra.length(); i++) {
-
-			for (int j = 0; j < 2; j++) {
-
-				resultado = resultado + palabra.charAt(i);
-
-			}
-		
-
-		}
-
-		return resultado;
 	}
 
-	private static int ocurrencias(String palabra, char letra) {
 
-		int contador = 0;
-
-		for (int i = 0; i < palabra.length(); i++) {
-
-			if (palabra.charAt(i) == letra) {
-
-				contador++;
-
-			}
-	
-		}
-
-		return contador;
-	}
 	public static void main(String[] args) {
-
-		System.out.println(repetirTodo("hola"));
-		System.out.print(ocurrencias("hola",'a'));
-		System.out.print(repetirVeces("hola",5));
+		// TODO Auto-generated method stub
+		System.out.println(mayus("hola"));
+		System.out.println(minus("HOLA"));
 	}
 }
+
