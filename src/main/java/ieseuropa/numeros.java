8e3239929c74ff956a64415b0a02afe5d0a5c83c
TilinExe816 <nuevaunion22@gmail.com>
2023-03-24 19:51:29 +0100

    Sucesion de Pell

---
+++ src/main/java/ieseuropa/numeros.java
@@ -0,0 +1,32 @@
+package main.java.ieseuropa;
+
+public class numeros {
+
+	private static String Pell() {
+
+		String resultado = "";
+
+		int numeroPrec = 2;
+
+		int numeroAnte = 1;
+
+		for (int i = 1; i < 16; i++) {
+
+			int aux = numeroAnte;
+
+			numeroAnte = numeroPrec;
+
+			numeroPrec = aux + (2 * (numeroPrec));
+
+			resultado = resultado + " " + numeroPrec;
+
+		}
+		return resultado;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Pell());
+	}
+
+}
