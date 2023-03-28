debe633511e63237454645763475d9e24a350cc2
TilinExe816 <nuevaunion22@gmail.com>
2023-03-28 17:09:22 +0200

    Primo

--- src/main/java/ieseuropa/numeros.java
+++ src/main/java/ieseuropa/numeros.java
@@ -3,36 +3,20 @@ package main.java.ieseuropa;
 
 public class numeros {
 
-	private static String Pell() {
-
-		String resultado = "";
-
-		
-		
-		int numeroPrec = 2;
-
-		int numeroAnte = 1;
-
-		resultado = resultado + "0 1 2";
-		
-		for (int i = 1; i < 14; i++) {
-
-			int aux = numeroAnte;
-
-			numeroAnte = numeroPrec;
-
-			numeroPrec = aux + (2 * (numeroPrec));
-
-			resultado = resultado + " " + numeroPrec;
-
-			
-		}
-		return resultado;
+	public static boolean Primo(int numero) {
+	  
+	    for (int i = 2; i <= Math.sqrt(numero); i++) {
+	        if (numero % i == 0) {
+	            return false;
+	        }
+	    }
+	    return true;
 	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Pell());
+		
 
 	}
 
