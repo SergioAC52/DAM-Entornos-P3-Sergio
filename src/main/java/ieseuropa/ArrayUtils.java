a654f4ed716d8f63294e3cf3331d18b1e076f794
Sergioac52 <sergio.antona1@educa.madrid.org>
2023-03-30 11:21:38 +0200

    Añadido OrdenarAsc

--- src/main/java/ieseuropa/ArrayUtils.java
+++ src/main/java/ieseuropa/ArrayUtils.java
@@ -1,5 +1,7 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+
 public class ArrayUtils {
 
 	
@@ -12,10 +14,19 @@ public class ArrayUtils {
 		}
 		return acumulador/cantDatos;
 	}
-	
+	public static int[] ordenarAsc(int[] array) {
+		Arrays.sort(array);
+		return array;
+	}
 	public static void main(String[] args) {
 		System.out.println(mediaArray(new int[] { 3, 14, 32, 1}));
 		System.out.println(mediaArray(new int[] {5 , 9 , 34 ,67}));
+		
+		int[] ordenar= new int[] { 5, 1, 4, 8, 2, 6};
+		ordenar = ordenarAsc(ordenar);
+		for (int i : ordenar) {
+			System.out.print(i+" ");
+		}
 	}
 
 }
